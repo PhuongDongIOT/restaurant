0874f58e24645194fc3c25df39299eee41f50281
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-04-11 15:27:09 +0700

    fix: delete res in service noti

--- service-notifications/src/app.module.ts
+++ service-notifications/src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MqttModule } from './mqtt/mqtt.module';
-import { RestModule } from './rest/rest.module';
 
 @Module({
-  imports: [MqttModule, RestModule],
+  imports: [MqttModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- service-notifications/src/mqtt/mqtt.service.ts
+++ service-notifications/src/mqtt/mqtt.service.ts
@@ -1,18 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
-import { Gpio } from 'onoff';
-let isOn = false;
 let intervalId;
 
 
 @Injectable()
 export class MqttService {
   private client: ClientProxy;
-  private readonly led: Gpio;
-  private readonly led1: Gpio;
-  private readonly led2: Gpio;
-  private readonly led3: Gpio;
-  private readonly led4: Gpio;
 
   constructor() {
     this.client = ClientProxyFactory.create({
@@ -21,11 +14,6 @@ export class MqttService {
         url: 'mqtt://localhost:1883', // Replace with your MQTT broker URL
       },
     });
-    this.led = new Gpio(18, 'out');
-    this.led1 = new Gpio(17, 'out');
-    this.led2 = new Gpio(22, 'out');
-    this.led3 = new Gpio(15, 'out');
-    this.led4 = new Gpio(25, 'out');
   }
 
   sendMessage(): void {
@@ -33,173 +21,65 @@ export class MqttService {
   }
 
   async turnOnLed() {
-    this.led.writeSync(1);
-    this.led1.writeSync(1);
-    this.led2.writeSync(1)
-    this.led3.writeSync(1)
-    this.led4.writeSync(1)
+    console.log("turnOnLed");
   }
 
   async blinkFast() {
     intervalId = setInterval(() => {
-      if (isOn) {
-        this.led.writeSync(1)
-        this.led1.writeSync(1)
-        this.led2.writeSync(0)
-        this.led3.writeSync(0)
-        this.led4.writeSync(1)
-        isOn = !isOn
-      } else {
-        this.led.writeSync(0)
-        this.led1.writeSync(0)
-        this.led2.writeSync(1)
-        this.led3.writeSync(1)
-        this.led4.writeSync(0)
-        isOn = !isOn
-      }
+    console.log("blinkFast");
     }, 1000)
   }
 
-  // async randomBlink() {
-  //   intervalId = setInterval(() => {
-  //     if (isOn) {
-  //       this.led.writeSync(1)
-  //       this.led1.writeSync(1)
-  //       this.led2.writeSync(0)
-  //       this.led3.writeSync(0)
-  //       this.led4.writeSync(1)
-  //       isOn = !isOn
-  //     } else {
-  //       this.led.writeSync(0)
-  //       this.led1.writeSync(0)
-  //       this.led2.writeSync(1)
-  //       this.led3.writeSync(1)
-  //       this.led4.writeSync(0)
-  //       isOn = !isOn
-  //     }
-  //   }, 1000)
-  // }
-
   async randomBlink() {
     intervalId = setInterval(() => {
-      const randomState = Math.random() > 0.5; // Generate a random boolean value
-
-      if (randomState) {
-        // Turn on a random combination of LEDs
-        this.turnOnRandomLEDs();
-      } else {
-        // Turn off all LEDs
-        this.turnOffLed();
-      }
+      console.log("randomBlink");
     }, 1000);
   }
 
   turnOnRandomLEDs() {
     // Assuming all LEDs are represented by an array
-    const leds = [this.led, this.led1, this.led2, this.led3, this.led4];
-
-    // Choose a random LED index
-    const randomIndex = Math.floor(Math.random() * leds.length);
-
-    // Turn off all LEDs
-    this.turnOffLed();
-
-    // Turn on the randomly selected LED
-    leds[randomIndex].writeSync(1);
+    console.log("turnOnRandomLEDs");
   }
 
 
 
   async blink(value: any) {
-    clearInterval(intervalId)
-    this.led1.writeSync(0)
-    intervalId = setInterval(() => {
-      if (isOn) {
-        this.led.writeSync(0)
-        this.led1.writeSync(0)
-        this.led2.writeSync(0)
-        this.led3.writeSync(0)
-        this.led4.writeSync(0)
-        isOn = !isOn
-      } else {
-        this.led.writeSync(1)
-        this.led1.writeSync(1)
-        this.led2.writeSync(1)
-        this.led3.writeSync(1)
-        this.led4.writeSync(1)
-        isOn = !isOn
-      }
-    }, value.data)
+    console.log("blink");
+    
   }
 
   async blinkSlow() {
     clearInterval(intervalId)
-    this.led1.writeSync(0)
-    intervalId = setInterval(() => {
-      if (isOn) {
-        this.led.writeSync(0)
-        this.led1.writeSync(0)
-        this.led2.writeSync(0)
-        this.led3.writeSync(0)
-        this.led4.writeSync(0)
-        isOn = !isOn
-      } else {
-        this.led.writeSync(1)
-        this.led1.writeSync(1)
-        this.led2.writeSync(1)
-        this.led3.writeSync(1)
-        this.led4.writeSync(1)
-        isOn = !isOn
-      }
-    }, 1000)
+    console.log("blinkSlow");
+    
   }
 
   async turnOffLed() {
-    this.led.writeSync(0);
-    this.led1.writeSync(0);
-    this.led2.writeSync(0);
-    this.led3.writeSync(0);
-    this.led4.writeSync(0)
+    console.log("turnOffLed");
     clearInterval(intervalId)
   }
 
   async blueLed() {
-    this.led.writeSync(1);
-    this.led1.writeSync(0);
-    this.led2.writeSync(0);
-    this.led3.writeSync(0);
-    this.led4.writeSync(0)
+    console.log("blueLed");
   }
 
   async greenLed() {
-    this.led.writeSync(0);
-    this.led1.writeSync(0);
-    this.led2.writeSync(0);
-    this.led3.writeSync(1);
-    this.led4.writeSync(0)
+    console.log("greenLed");
+    
   }
 
   async redLed() {
-    this.led.writeSync(0);
-    this.led1.writeSync(0);
-    this.led2.writeSync(0);
-    this.led3.writeSync(0);
-    this.led4.writeSync(1)
+    console.log("redLed");
+    
   }
   async yellowLed() {
-    this.led.writeSync(0);
-    this.led1.writeSync(1);
-    this.led2.writeSync(0);
-    this.led3.writeSync(0);
-    this.led4.writeSync(0)
+    console.log("yellowLed");
+    
   }
 
   async whiteLed() {
-    this.led.writeSync(0);
-    this.led1.writeSync(0);
-    this.led2.writeSync(1);
-    this.led3.writeSync(0);
-    this.led4.writeSync(0)
+    console.log("whiteLed");
+    
   }
 
 }
