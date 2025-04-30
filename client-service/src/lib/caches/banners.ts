25e9716645ff82e63b0d4905dd34b95ecea0d767
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-04-30 23:51:34 +0700

    init

--- client-service/src/lib/caches/banners.ts
+++ client-service/src/lib/caches/banners.ts
@@ -5,7 +5,9 @@ export async function fetchAndCacheBanners(kv: any, bannerService: any) {
   const bannersFromKV = await getKVData<ICategoryBanner[]>('banners', [], kv);
 
   if (bannersFromKV.length === 0) {
-    const { data } = await bannerService.filters();
+    const { data } = await bannerService.filters({
+      queryParams: { page: 1 },
+  });
     await kv.set('banners', JSON.stringify(data));
     return data;
   }
