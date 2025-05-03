a012cad2bd30b614c0c68cb0a29f67efdf00dfa2
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-05-03 18:52:03 +0700

    init

--- client-service/src/app/(lobby)/page.tsx
+++ client-service/src/app/(lobby)/page.tsx
@@ -1,38 +1,15 @@
 import { kv } from '@vercel/kv';
 import dynamic from 'next/dynamic';
-import { ProductGalleryProps } from './_components/product-gallery';
 import { fetchAndCacheBanners } from '@/lib/caches/banners';
 import { fetchAndCacheProducts } from '@/lib/caches/products';
+import LazyPage from './_components/lazy-page';
+import type { ProductGalleryProps } from './_components/product-gallery';
 
 const Hero = dynamic(() => import('./_components/hero'), {
   ssr: true,
   loading: () => <p>Loading...</p>,
 })
-const ProductCategory = dynamic(() => import('./_components/product-category'), {
-  ssr: true,
-  loading: () => <p>Loading...</p>,
-})
 const ProductTrend = dynamic(() => import('./_components/product-trend'), {
-  ssr: true,
-  loading: () => <p>Loading...</p>,
-})
-const CategoryTrend = dynamic(() => import('./_components/category-trend'), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-})
-const ProductCommon = dynamic(() => import('./_components/product-common'), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-})
-const BannerFooter = dynamic(() => import('./_components/banner-footer'), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-})
-const MapBox = dynamic(() => import('./_components/map-box'), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-})
-const ProductGallery = dynamic(() => import('./_components/product-gallery'), {
   ssr: true,
   loading: () => <p>Loading...</p>,
 })
@@ -42,7 +19,7 @@ const initProductGallery: ProductGalleryProps = {
   description: "Món ăn cao cấp, xa xỉ, sử dụng nguyên liệu quý hiếm và có cách chế biến cầu kỳ, tinh tế.",
   products: []
 }
-// Main logic
+
 async function initData(kv: any) {
   const products = await fetchAndCacheProducts(kv, 1);
   const banners = await fetchAndCacheBanners(kv, 1);
@@ -53,18 +30,12 @@ async function initData(kv: any) {
 
 export default async function Page() {
   const { banners, initPGOne } = await initData(kv);
-
   return (
     <>
       <Hero items={banners} />
       <div className='py-8 flex flex-col gap-12 mx-auto max-w-6xl'>
         <ProductTrend {...initPGOne} title='Món ăn đặc sắc' />
-        <CategoryTrend  {...initPGOne} title='Danh mục phổ biến' />
-        <ProductCategory {...initPGOne} title='Món ăn phổ biến' />
-        <ProductCommon  {...initPGOne} title='Món ăn phổ biến' />
-        <ProductGallery {...initPGOne} />
-        <BannerFooter src="/images/banner-hot-port.webp" />
-        <MapBox />
+        <LazyPage  initPGOne={initPGOne} />
       </div>
     </>
   );
