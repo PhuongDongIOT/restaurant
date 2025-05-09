6ff775927c2590e3f69de97327d0b1d090d14615
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-05-09 21:12:16 +0700

    init

--- client-service/src/app/dashboard/blog/new/page.tsx
+++ client-service/src/app/dashboard/blog/new/page.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from 'react';
-import FormCardSkeleton from '@/components/molecules/form-card-skeleton';
 import PageContainer from '@/components/layout/page-container';
 import BlogCreatePage from '@/modules/blog/pages/blog-create.page';
 
@@ -7,9 +5,7 @@ export default async function Page() {
   return (
     <PageContainer scrollable>
       <div className='flex-1 space-y-4'>
-        <Suspense fallback={<FormCardSkeleton />}>
           <BlogCreatePage />
-        </Suspense>
       </div>
     </PageContainer>
   );
