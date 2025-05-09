35fd259fb3a312411736bd10b752ed92e8ba6e9d
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-05-09 21:01:17 +0700

    init

--- client-service/src/app/dashboard/blog/new/page.tsx
+++ client-service/src/app/dashboard/blog/new/page.tsx
@@ -1,19 +1,9 @@
 import { Suspense } from 'react';
 import FormCardSkeleton from '@/components/molecules/form-card-skeleton';
 import PageContainer from '@/components/layout/page-container';
-import { categoryService } from '@/lib/services/category.service';
 import BlogCreatePage from '@/modules/blog/pages/blog-create.page';
 
-export const metadata = {
-  title: 'Bảng điều khiển: Xem sản phẩm'
-};
-
-type PageProps = { params: Promise<{ productId: string }> };
-
-export default async function Page(props: PageProps) {
-  const params = await props.params;
-  const { data: categories } = await categoryService.filters();
-
+export default async function Page() {
   return (
     <PageContainer scrollable>
       <div className='flex-1 space-y-4'>
